e43441a86eea8210231eb76b0128ac7a92adb839
Silas Owings <silasowings@gmail.com>
2018-08-14 14:42:02 -0500

    error handling
    some basic errorhandling for googleapi requests

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}`,
   json: true
 }, (error, response, body) => {
+  if (error){
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    //googleapi returns status of 'ZERO RESULTS' if it cant locate an address
+    console.log('Address not found.');
+  } else if (body.status === 'OK') {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${JSON.stringify(body.results[0].geometry.location.lat, undefined, 2)}`);
   console.log(`Longitude: ${JSON.stringify(body.results[0].geometry.location.lng, undefined, 2)}`);
+}
 });
